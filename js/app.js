7781312b7ab667c523a0af8669d49acef86e1285
Vineeth Vijayan <vinbhai4u@gmial.com>
2014-06-08 15:34:58 +0530

    backbone commit

--- js/app.js
+++ js/app.js
@@ -1,53 +1,17 @@
-/**
- * Demo App for TopcoatTouch
- */
-$(document).ready(function() {
+var app = {
+    views: {},
+    models: {},
+    routers: {},
+    utils: {},
+    adapters: {}
+};
 
-
-    // Create the topcoatTouch object
-    var tt = new TopcoatTouch();
-
-    // First page we go to home...  This could be done in code by setting the class to 'page page-center', but here is how to do it in code...
-
-    $('#home-next-btn').click(function() {
-        // toDoService.addToDo($('#nameInput').val(), $('#detailsTextarea').val(), $('#dueDateInput').val());
-        // toDoService.save();
-        // createToDoList();
-        tt.goTo('#details');
-    });
-
-    tt.goTo('home');
-
-
-
+$(document).on("ready", function () {
+    app.router = new app.routers.AppRouter();
+    app.utils.templates.load(["HomeView", "SecondPageView", "EmployeeView", "EmployeeListItemView", "ReportsView", "MapView"],
+        function () {
+            app.router = new app.routers.AppRouter();
+            Backbone.history.start();
+        });
 });
 
-function showVal (rangeValue) {
-	$('#mainsmiley').removeClass();
-	$('#mainsmiley').addClass('all-center')
-	if(rangeValue<15)
-	{
-		$('#mainsmiley').addClass('angry');
-		// $('#mainrangeslider').val(0);	
-	}
-	else if(rangeValue<45)
-	{	
-		$('#mainsmiley').addClass('sad');
-		// $('#mainrangeslider').val(25);
-	}
-	else if(rangeValue<75)
-	{	
-		$('#mainsmiley').addClass('neutral');
-		// $('#mainrangeslider').val(50);
-	}
-	else if(rangeValue<85)
-	{	
-		$('#mainsmiley').addClass('smiley');
-		// $('#mainrangeslider').val(75);
-	}
-	else 
-	{
-		$('#mainsmiley').addClass('happy');
-		// $('#mainrangeslider').val(100);
-	}
-}
---
+++ js/views/HomeView.js
@@ -0,0 +1,54 @@
+app.views.HomeView = Backbone.View.extend({
+
+    initialize: function () {
+        this.searchResults = new app.models.EmployeeCollection();
+        this.searchresultsView = new app.views.EmployeeListView({model: this.searchResults});
+    },
+
+    render: function () {
+        this.$el.html(this.template());
+        $('.scroller', this.el).append(this.searchresultsView.render().el);
+        return this;
+    },
+
+    events: {
+        "input #main-slider": "handleSliderChange",
+        "click #home-next-btn": "secondPage"
+    },
+
+    handleSliderChange: function (e) {
+        var rangeValue = $('#main-slider').val();
+        $('#mainsmiley').removeClass();
+        $('#mainsmiley').addClass('all-center')
+        if(rangeValue<15)
+        {
+            $('#mainsmiley').addClass('angry');
+            // $('#mainrangeslider').val(0);    
+        }
+        else if(rangeValue<45)
+        {   
+            $('#mainsmiley').addClass('sad');
+            // $('#mainrangeslider').val(25);
+        }
+        else if(rangeValue<75)
+        {   
+            $('#mainsmiley').addClass('neutral');
+            // $('#mainrangeslider').val(50);
+        }
+        else if(rangeValue<85)
+        {   
+            $('#mainsmiley').addClass('smiley');
+            // $('#mainrangeslider').val(75);
+        }
+        else 
+        {
+            $('#mainsmiley').addClass('happy');
+            // $('#mainrangeslider').val(100);
+        }
+    },
+
+    secondPage: function (argument) {
+        window.location("#secondPage");
+    }
+
+});
