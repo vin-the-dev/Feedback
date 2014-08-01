98cdd486c73804b2ee7e5165eeb992fed39bbc51
Vineeth Vijayan <vinbhai4u@gmial.com>
2014-08-01 23:26:02 +0530

    Before Changing to dynamic questions

---
+++ php/connect.php
@@ -0,0 +1,6 @@
+<?PHP
+mysqli_connect("109.203.122.141","vin","joker@1231","feedback");
+
+or die(mysql_error());
+
+?>
---
+++ php/questions.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?PHP
+include("connect.php");
+echo("hi");
+?>
+</body>
+</html>
